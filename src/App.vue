ba5c4eefef206dc6487089b4de54e5ec7d0fd3eb
nightwarelisten <18790277053@163.com>
2018-08-17 19:01:53 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" @click="getSeller" >
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import VueResource from 'vue-resource'
+export default {
+    data(){
+        return {
+            seller :"23"
+        }
+    },
+    methods:{
+        getSeller(){
+          this.$http.get('/api/seller').then((response) => {
+            response = response.body;
+            console.log(response,234)
+          });
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
